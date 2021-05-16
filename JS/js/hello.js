32089e7333947c537bcc2e5dc7228496865eb652
josan <josanescod@gmail.com>
2021-05-16 07:51:36 +0200

    js

--- JS/js/hello.js
+++ JS/js/hello.js
@@ -1,8 +1,11 @@
-alert ('Hello World');
+/*alert ('Hello World');
 console.log('Hello World');
-
 console.log ("Hello World");
 console.log(3+3);
 console.log (typeof(true));
-console.log(typeof(87));
+console.log(typeof(87));*/
 
+let name1 = "Jane";
+console.log(name1);
+var name2 = prompt("What is her name? ");
+console.log("Her name is "+ name2);
