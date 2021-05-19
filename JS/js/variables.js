6f29c97259d67c56f3353a17fc564b6caabe1208
josan <josanescod@gmail.com>
2021-05-19 09:02:31 +0200

    js variables

--- JS/js/variables.js
+++ JS/js/variables.js
@@ -9,3 +9,9 @@ b=temp;
 
 console.log("a is "+a+' '+typeof(a));
 console.log("b is "+b+' '+typeof(a));
+
+/* Rules  naming variables */
+let myName = "Test1";
+let my123 = 123;
+let my_name= "name1";
+let userScoreFinal=90;
