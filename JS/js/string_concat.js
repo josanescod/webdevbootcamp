93b3a998a5a690b79d641a207d7eedde781219d7
josan <josanescod@gmail.com>
2021-05-20 07:30:05 +0200

    js string concatenation

---
+++ JS/js/string_concat.js
@@ -0,0 +1,9 @@
+console.log('string concatenation');
+console.log('a'+' '+'b');
+console.log ('hello'+' '+'world' );
+
+let message = "Hello";
+let randomName = "Spike";
+
+
+console.log(message+' '+randomName);
