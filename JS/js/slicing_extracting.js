08dfd49de564c64b1c9b4c76d6aba54b31f1b522
josan <josanescod@gmail.com>
2021-05-23 11:53:49 +0200

    js

--- JS/js/slicing_extracting.js
+++ JS/js/slicing_extracting.js
@@ -1,11 +1,11 @@
 
 // slice
-let oneName = "Marcial";
-
+/*let oneName = "Marcial";
 console.log(oneName.slice(0, 1));
-
 console.log(oneName.slice(2, 6));
-
 console.log(oneName.slice(0, 6));
+console.log(oneName.slice(6, 7));*/
 
-console.log(oneName.slice(6, 7));
+let tweet = prompt("Compose your tweet: ");
+let tweetUnder140 = tweet.slice(0,20);
+alert(tweetUnder140);
