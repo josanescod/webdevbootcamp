2c74a38c52deece28f50af39e92ed476bbdc5b1f
josan <josanescod@gmail.com>
2021-05-23 11:48:28 +0200

    js

---
+++ JS/js/slicing_extracting.js
@@ -0,0 +1,11 @@
+
+// slice
+let oneName = "Marcial";
+
+console.log(oneName.slice(0, 1));
+
+console.log(oneName.slice(2, 6));
+
+console.log(oneName.slice(0, 6));
+
+console.log(oneName.slice(6, 7));
