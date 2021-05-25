37390396324e96ae8c3e89958276920e2bc2c857
josan <josanescod@gmail.com>
2021-05-25 10:14:32 +0200

    js

--- JS/js/upperlowercase.js
+++ JS/js/upperlowercase.js
@@ -1,8 +1,24 @@
 
-let dogName = 'rufus';
+/*let dogName = 'rufus';
 let dogCat = "SALLY";
-
-
 dogCat = dogCat.toLowerCase();
 console.log(dogName.toUpperCase());
-console.log(dogCat);
+console.log(dogCat);*/
+
+//1 create a var that stores the name that user enters via prompt
+let aName = prompt("What is your name?");
+
+//2 Capitalize the first letter of their name.
+console.log(aName);
+
+//a isolate the first char
+let firstChar = aName.slice(0,1);
+//b turn the first char to upper case
+let upperCaseFirstChar = firstChar.toUpperCase();
+//c Isolate the rest of the name
+let restOfName = aName.slice(1,aName.length);
+//3 We use the capitalized version of their name to greet them using an alert.
+let capitaliseName = upperCaseFirstChar + restOfName;
+//show complete name
+alert ("Hello, "+ capitaliseName);
+
