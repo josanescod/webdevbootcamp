46585a241dfc94a6c18e1a83e208ae16793e8daa
josan <josanescod@gmail.com>
2021-05-27 09:03:15 +0200

    js

---
+++ JS/js/arithmetic.js
@@ -0,0 +1,13 @@
+let a = 2 + 4;
+console.log(a);
+let b = 7/2;
+console.log(b);
+let c = 2*3;
+console.log(c);
+let d = 8-2;
+console.log(d);
+let e = 8%2;
+console.log('modulo 9%2 = '+e);
+let f = 12%8;
+console.log('modulo 12%8= '+f);
+// simbols order 
