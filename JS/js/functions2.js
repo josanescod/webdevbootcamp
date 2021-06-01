bb0432040699bffab82d285f56fb5e26e63b6f13
josan <josanescod@gmail.com>
2021-06-01 06:43:10 +0200

    js

--- JS/js/functions2.js
+++ JS/js/functions2.js
@@ -1,40 +1,42 @@
 // Parameters and arguments
 
-getMilk();
+getMilk(5);
 
 
-function getMilk() {   
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-  }
+//cost of bottle 1.5€
+function getMilk(bottles) {
+  let cost = bottles * 1.5;
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight")
+  console.log("cost of -> "+bottles+" bottle(s) is, "+cost+" euros")
+  console.log("buy "+ bottles + " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
 
+function getMilk2(bottles) {
+  console.log('buy ' + bottles + ' bottles of Milk');
 
-  function getMilk2(bottles) {
-    console.log('buy '+ bottles + ' bottles of Milk');
+}
 
-  }
+//calling function getMilk3(2);
 
-
-  //calling function getMilk3(2);
-  
-  function getMilk3(bottles) {
-    var cost = bottles * 1.5;
-    //Do something with cost
-    console.log('cost: '+cost);
-  }
+function getMilk3(bottles) {
+  var cost = bottles * 1.5;
+  //Do something with cost
+  console.log('cost: ' + cost);
+}
