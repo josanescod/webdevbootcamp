6504f7ade6e2353b3736282ca74dfe28d8cee9b0
josan <josanescod@gmail.com>
2021-06-03 08:46:00 +0200

    js

--- JS/js/liveinweeks.js
+++ JS/js/liveinweeks.js
@@ -1,11 +1,16 @@
-lifeInWeeks(age);
+lifeInWeeks(45);
 
 function lifeInWeeks(age) {
     
+    let finishYear = 90;
+
     let days;
     let weeks;
     let months;
+    days = (finishYear * 365) - (age * 365);
+    weeks = (finishYear * 52) - (age * 52);
+    months = (finishYear * 12) - (age * 12);    
 
-    console.log('You have '+days+' days, '+weeks+' weeks, and '+monts+' months left.');
+    console.log('You have '+days+' days, '+weeks+' weeks, and '+months+' months left.');
 
 }
