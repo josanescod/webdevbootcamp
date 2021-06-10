d536fef1984baf05e37dc2baefcd686ea26410c8
josan <josanescod@gmail.com>
2021-06-10 06:28:57 +0200

    js

--- JS/js/functions5.js
+++ JS/js/functions5.js
@@ -1,9 +1,5 @@
 /*BMI Calculator  BMI= weight (kg)/height^2(m^2)
 If my weight is 65kg and my height is 1.8m, I should be able to call your function like this:
-
-
 let bmi = bmiCalculator(65,1.8);
-
 bmi should equal around 20 in this case.
-
 */
