676122a1de1d4101d1bc88aca85bbfbc8361a0f2
josan <josanescod@gmail.com>
2021-06-15 08:04:04 +0200

    js

--- JS/js/leapYear.js
+++ JS/js/leapYear.js
@@ -1,1 +1,11 @@
-console.log("leapyear..")
+/*
+Write a program that works out whether if a given year is a leap year. A normal year has 365 years days,
+leap years have 366, with and extra day in February. The reason why we have leap years is really fascinating. 
+This is how to work out whether if a particular year is a leap year.
+on every year that is evenly divisible by 4
+except every year that is evenly divisible by 100
+unless the year is also evenly divisible by 400
+*/ 
+function isLeap(year) {
+
+}
