50dcfd1c6501f25a238cd9334e8506f89c7b52d5
josan <josanescod@gmail.com>
2021-06-16 09:38:49 +0200

    js

--- JS/js/arrays.js
+++ JS/js/arrays.js
@@ -1,1 +1,15 @@
-console.log("arrays..");
+/* colections of data */ 
+
+
+
+
+let guestList = ["Ronaldo", "Paco" ,"Santiago", "Maria", "Ana"];
+
+let guestName = prompt ("What is your name? ");
+
+if (guestList.includes(guestName)) {
+    alert ("Welcome to the party!");
+}else {
+
+    alert ("Sorry, maybe next time.");
+}
