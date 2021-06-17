f272dc6cf6390e30efd8310930ac94b3cc94763a
josan <josanescod@gmail.com>
2021-06-17 07:31:11 +0200

    js

--- JS/js/whosPaying.js
+++ JS/js/whosPaying.js
@@ -1,20 +1,14 @@
 let friends = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
 
-
 function whosPaying(names) {
 
     let numberOfPeople = names.length;
-
     let randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
-
-    let randomPerson = names[randomPersonPosition] 
+    let randomPerson = names[randomPersonPosition]
 
     return randomPerson + " is going to buy lunch today!";
 
-
 }
-
-
 
 let test = whosPaying(friends);
 console.log(test);
