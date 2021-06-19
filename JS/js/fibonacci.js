5a652bc9b9a33d29ddafb56381f8ce02b47574ed
josan <josanescod@gmail.com>
2021-06-19 07:25:17 +0200

    js

--- JS/js/fibonacci.js
+++ JS/js/fibonacci.js
@@ -1,1 +1,23 @@
-console.log('load..');
+
+function fibonacciGenerator(n) {
+
+    let output = [];
+    if (n === 1) {
+        output = [0];
+    } else if (n === 2){
+        output = [0,1];
+    }else {
+        output = [0,1];
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output [output.length - 1]); // [0, 1, 1]
+
+        }
+        
+       
+    }
+    return output;
+}
+
+output = fibonacciGenerator(4);
+console.log(output);
+
