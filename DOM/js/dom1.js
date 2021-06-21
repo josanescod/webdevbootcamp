6bf0e2b157183227510dbfd2fc89bdd79cc5f5c1
josan <josanescod@gmail.com>
2021-06-21 08:29:08 +0200

    js

--- DOM/js/dom1.js
+++ DOM/js/dom1.js
@@ -1,3 +1,19 @@
-//alert ("Hello to Bye");
 
-//document.querySelector("h1").innerHTML = "Good Bye";
+//selecting HTML elements with Javascript
+
+let liElements = document.getElementsByTagName('li');
+console.log(liElements[0]);
+
+let button = document.getElementsByClassName('btn');
+console.log(button);
+
+let list = document.getElementById('title');
+list.style.color = 'orange';
+
+let title = document.querySelector('#title');
+console.log(title.textContent);
+
+let titles = document.querySelectorAll("#list .item");
+console.log(titles);
+
+document.querySelectorAll("#list .item")[1].style.color = "green";
