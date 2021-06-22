ba954b1b0fbd84f4264887b454e9a561b5d7b8da
josan <josanescod@gmail.com>
2021-06-22 06:43:42 +0200

    js

---
+++ DOM/js/dom2.js
@@ -0,0 +1,9 @@
+/*
+Separation of concerns: Structure, Style and Behaviour;
+html, css, js
+*/
+
+let clickMe = document.querySelector(".btn");
+console.log(clickMe.classList);
+clickMe.classList.add("invisible");
+console.log(clickMe.classList);
