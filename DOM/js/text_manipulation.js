6bf9edcb38e72cdfd173eaf66cbf28d0cb6d7748
josan <josanescod@gmail.com>
2021-06-23 06:17:44 +0200

    js

--- DOM/js/text_manipulation.js
+++ DOM/js/text_manipulation.js
@@ -1,3 +1,8 @@
-console.log('loading');
+
+/*
+innetHtml / textContent
+*/
+
 
 document.querySelector('h1').innerHTML = "<em><span class='main_title'>Hello</span></em>";
+document.querySelector('button').textContent = "Send Mail";
