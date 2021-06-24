ff9ab360e5fb542d71f58e7f32fa240b62618f7f
josan <josanescod@gmail.com>
2021-06-24 09:31:38 +0200

    js

--- Dice_challenge/js/index.js
+++ Dice_challenge/js/index.js
@@ -7,19 +7,23 @@ let player1 = randomNumberGenerator(6);
 let player2 = randomNumberGenerator(6);
 let img1 = document.querySelector('.img1');
 let img2 = document.querySelector('.img2')
-//console.log('player1: ' + randomNumber1, 'player2: ' + randomNumber2);
-img1.setAttribute('src', `images/dice${player1}.png`);
-img2.setAttribute('src', `images/dice${player2}.png`);
-/*console.log(document.querySelector('.img1'));
-console.log(document.querySelector('.img2'));*/
-
+let randomImageSource1 = `images/dice${player1}.png`;
+let randomImageSource2 = `images/dice${player2}.png`;
 let displayWinner = document.querySelector('h1');
+
+img1.setAttribute('src', randomImageSource1);
+img2.setAttribute('src', randomImageSource2);
+
 if (player1 > player2) {
+
     displayWinner.innerHTML = 'Player 1 wins!';
-}else if (player2 > player1) {
+
+} else if (player2 > player1) {
+
     displayWinner.innerHTML = 'Player 2 wins!';
 
-}else{
+} else {
+
     displayWinner.innerHTML = 'Draw!';
 
 }
