ee01f61d4e72cbe612214df2659e243789876a94
josan <josanescod@gmail.com>
2021-06-25 08:53:08 +0200

    js

--- Drum_kit/js/index.js
+++ Drum_kit/js/index.js
@@ -1,11 +1,11 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+for (let i = 0; i < numberOfDrumButtons; i++) {
 
+    document.querySelectorAll('.drum')[i].addEventListener("click", () => {
 
-for (let i = 0; i < numberOfDrumButtons; i++){
-    
-    document.querySelectorAll('.drum')[i].addEventListener("click",function(){
         alert("I got clicked!");
+
     });
 }
 
