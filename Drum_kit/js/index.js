b7ebd7d402994450ab706a4fde3db631bcb4e48f
josan <josanescod@gmail.com>
2021-06-26 07:06:39 +0200

    js

--- Drum_kit/js/index.js
+++ Drum_kit/js/index.js
@@ -16,32 +16,44 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
         switch (this.innerHTML) {
             case 'w':
                 console.log('has pulsado w');
-                let audio = new Audio('sounds/crash.mp3');
-                audio.play();
+                let tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
                 break;
 
             case 'a':
                 console.log('has pulsado a');
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
                 break;
 
             case 's':
                 console.log('has pulsado s');
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
                 break;
 
             case 'd':
                 console.log('has pulsado d');
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
                 break;
 
             case 'j':
                 console.log('has pulsado j');
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
                 break;
 
             case 'k':
                 console.log('has pulsado k');
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
                 break;
          
             case 'l':
                 console.log('has pulsado l');
+                let kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
                 break;
 
             default:
