7cf4daac545f901425d0c18c7fa8a3beed02da27
josan <josanescod@gmail.com>
2021-06-26 06:40:59 +0200

    js

--- Drum_kit/js/index.js
+++ Drum_kit/js/index.js
@@ -2,10 +2,17 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
 
-    document.querySelectorAll('.drum')[i].addEventListener("click", () => {
+    document.querySelectorAll('.drum')[i].addEventListener("click",  function() {
 
-        let audio = new Audio ('sounds/tom-1.mp3')
-        audio.play();
+       console.log(this.innerHTML);
+
+       if (this.innerHTML === 'w') {
+           console.log('has pulsado w');
+       }
+
+       //change style color
+       this.style.color = 'white';
+        
 
     });
 }
