eb7c0d41ffb0bc094a71a4d80b36258360eeeb51
josan <josanescod@gmail.com>
2021-06-26 09:26:28 +0200

    js

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,10 @@
-console.log("loading...");
-$('h1').html("Hello World with jQuery!")
-       .css("color","blue");
 
+$('h1').html("Hello World with jQuery!")
+       .css("color","blue")
+       .addClass("big-title");
+
+$('button').eq(2).css("color","orange");
+
+$('button').first().css("color","green");
+
+$('button').last().css("font-size","0.5rem");
