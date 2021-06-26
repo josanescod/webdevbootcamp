4bae5f0dee8dd8ea66b0011dbe23c6cd6fea3f9b
josan <josanescod@gmail.com>
2021-06-26 07:01:10 +0200

    js

--- Drum_kit/js/index.js
+++ Drum_kit/js/index.js
@@ -2,17 +2,52 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
 
-    document.querySelectorAll('.drum')[i].addEventListener("click",  function() {
+    document.querySelectorAll('.drum')[i].addEventListener("click", function () {
 
-       console.log(this.innerHTML);
-
-       if (this.innerHTML === 'w') {
-           console.log('has pulsado w');
-       }
-
-       //change style color
-       this.style.color = 'white';
+        /*console.log(this.innerHTML);
+ 
+        if (this.innerHTML === 'w') {
+            console.log('has pulsado w');
+        }
+ 
+        //change style color
+        this.style.color = 'white';*/
         
+        switch (this.innerHTML) {
+            case 'w':
+                console.log('has pulsado w');
+                let audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+
+            case 'a':
+                console.log('has pulsado a');
+                break;
+
+            case 's':
+                console.log('has pulsado s');
+                break;
+
+            case 'd':
+                console.log('has pulsado d');
+                break;
+
+            case 'j':
+                console.log('has pulsado j');
+                break;
+
+            case 'k':
+                console.log('has pulsado k');
+                break;
+         
+            case 'l':
+                console.log('has pulsado l');
+                break;
+
+            default:
+                console.log('ok');
+        }
+
 
     });
 }
