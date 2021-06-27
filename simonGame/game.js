6fb2718f9bf67389eaa90d2878d36351b06bc8df
josan <josanescod@gmail.com>
2021-06-27 09:22:29 +0200

    js

--- simonGame/game.js
+++ simonGame/game.js
@@ -5,27 +5,35 @@ let userClickedPattern = [];
 
 $(".btn").click(function () {
     let userChosenColour = $(this).attr("id")
-    console.log(userChosenColour);
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 
 });
 
 function nextSequence() {
 
     let randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
     let randomChosenColour = buttonColours[randomNumber];
-    console.log(randomChosenColour);
     gamePattern.push(randomChosenColour);
     console.log(gamePattern);
     $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    /*let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();*/
-
-
-
+    playSound(randomChosenColour);
 }
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $('#' + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $('#' + currentColour).removeClass("pressed");
+    }, 100);
+}
+
 nextSequence();
 
 
