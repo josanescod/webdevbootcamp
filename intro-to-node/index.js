ff07a70bbff88b37d87d1b59744a75dd174ef9de
josan <josanescod@gmail.com>
2021-06-28 07:10:34 +0200

    nodejs

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,6 +1,10 @@
 //const fs = require("fs");
 //const superhero = require ("superhero-name-library");
 const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
 let mySuperheroName = superheroes.random();
+let superVillain = supervillains.random();
 
 console.log(mySuperheroName);
+console.log(superVillain);
