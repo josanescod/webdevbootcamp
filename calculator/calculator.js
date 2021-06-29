881c4c2c30c3841cfe8906c71a7b01c6522eb9fe
josan <josanescod@gmail.com>
2021-06-29 09:42:28 +0200

    nodejs

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,16 @@
 const express = require("express");
 require("dotenv").config();
+const path = require('path');
 
 const app = express();
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get("/",(req,res)=>{
     
-    res.send("calculator app");
+    //res.send("calculator app");
+    console.log(__dirname);
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(process.env.PORT,()=>{
