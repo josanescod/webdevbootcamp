3f78051bd1d1c5b5d363dd54eaaf1e278727a160
josan <josanescod@gmail.com>
2021-06-29 08:54:51 +0200

    nodejs

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,16 +5,16 @@ const app = express();
 
 app.get("/",(req,res)=>{
     
-    res.send("<h1>node app</h1>");
+    res.send("<h1>My first node app</h1>");
 });
 
-app.get("/hello",(req,res)=>{
-    res.send(`<h2 style="color:red">Hello</h2>
-<h3 style="color:green">Hello</h3>`);
+app.get("/portfolio",(req,res)=>{
+    res.send(`<h2 style="color:red">There are some of my projects</h2>
+<h3 style="color:green">Web app 1</h3>`);
 });
 
-app.get("/goodbye",(req,res)=>{
-    res.send("Good bye");
+app.get("/contact",(req,res)=>{
+    res.send("Contact me at: name@gmail.com");
 });
 
 
