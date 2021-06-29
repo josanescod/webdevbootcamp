6c244b84c12d034f053be3906ae5dd88f1e321a4
josan <josanescod@gmail.com>
2021-06-29 07:04:56 +0200

    nodejs

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 7000;
+
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    
+    res.send("<h1>node app</h1>");
+});
+
+app.get("/hello",(req,res)=>{
+    res.send(`<h2 style="color:red">Hello</h2>
+<h3 style="color:green">Hello</h3>`);
+});
+
+app.get("/goodbye",(req,res)=>{
+    res.send("Good bye");
 });
 
 
-app.listen(port,()=>{
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT,()=>{
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
