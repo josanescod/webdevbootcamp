ea7de2f46758b42a8b49bb175cd23cc4bd5e20ab
josan <josanescod@gmail.com>
2021-06-30 12:32:14 +0200

    nodeAPI

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 require("dotenv").config();
-
+const path = require('path');
+const https = require("https");
 
 const app = express();
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));//POST
+app.get("/", function(req, res){       
+         
+        res.sendFile(__dirname + "/signup.html");
+    });
+
+app.post("/",function(req,res){
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let email = req.body.email;
+    console.log(firstname,lastname,email);
+})
 
 app.listen(process.env.PORT,function(){
     console.log(`Server listening on port ${process.env.PORT}`);
