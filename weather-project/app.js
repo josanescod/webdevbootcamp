dc032a1f22672ded85608c27b5e93d555e431cd0
josan <josanescod@gmail.com>
2021-06-30 10:24:47 +0200

    nodeAPI

--- weather-project/app.js
+++ weather-project/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 require("dotenv").config();
 const https = require("https");
+const path = require('path');
 
 const app = express();
 
+app.use('/static', express.static(path.join(__dirname, 'assets')));
+
+app.use(express.urlencoded({ extended: true }));//POST
+
+
 app.get("/", function(req, res){
-    const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=London&aqi=no`;
+
+    res.sendFile(__dirname + "/index.html");
+
+    /*const city = "Madrid";
+    const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=${city}&aqi=no`;
     https.get(url,function(response){       
         response.on("data", function(data){          
             const weatherData = JSON.parse(data)   
@@ -19,8 +29,30 @@ app.get("/", function(req, res){
             res.write(`<img src='${imgURL}' alt="weather icon">`);
             res.send();
         })
-    })
+    })*/
    
+});
+
+app.post("/",function (req,res) {
+    console.log("Post request recieved");
+    console.log(req.body.cityName);
+    const city = req.body.cityName
+    const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=${city}&aqi=no`;
+    https.get(url,function(response){       
+        response.on("data", function(data){          
+            const weatherData = JSON.parse(data)   
+            console.log(weatherData);      
+            const location = weatherData.location.name;
+            const condition = weatherData.current.condition.text
+            const temp = weatherData.current.temp_c;
+            const imgURL = weatherData.current.condition.icon;
+            res.write("<h1>The wheather in mi city</h1><br>")
+            res.write(`<p>the temperature in ${location} is: ${temp} degrees Celsius and the condition is ${condition}</p>`);
+            res.write(`<img src='${imgURL}' alt="weather icon">`);
+            res.write(`<a href="${process.env.URL}">Back</a>`)
+            res.send();
+        })
+    })
 });
 
 app.listen(process.env.PORT,function(){
