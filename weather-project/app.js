7cec92c513a948c3288c5e8e3e0123c46262cf0c
josan <josanescod@gmail.com>
2021-06-30 10:35:44 +0200

    nodeAPI

--- weather-project/app.js
+++ weather-project/app.js
@@ -11,29 +11,14 @@ app.use(express.urlencoded({ extended: true }));//POST
 
 
 app.get("/", function(req, res){
-
+    
     res.sendFile(__dirname + "/index.html");
 
-    /*const city = "Madrid";
-    const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=${city}&aqi=no`;
-    https.get(url,function(response){       
-        response.on("data", function(data){          
-            const weatherData = JSON.parse(data)   
-            console.log(weatherData);      
-            const location = weatherData.location.name;
-            const condition = weatherData.current.condition.text
-            const temp = weatherData.current.temp_c;
-            const imgURL = weatherData.current.condition.icon;
-            res.write("<h1>The wheather in mi city</h1><br>")
-            res.write(`<p>the temperature in ${location} is: ${temp} degrees Celsius and the condition is ${condition}</p>`);
-            res.write(`<img src='${imgURL}' alt="weather icon">`);
-            res.send();
-        })
-    })*/
    
 });
 
 app.post("/",function (req,res) {
+
     console.log("Post request recieved");
     console.log(req.body.cityName);
     const city = req.body.cityName
