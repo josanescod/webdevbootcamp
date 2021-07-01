e258bbfde7b55e3e4d768f8f82c2a292dfd4a313
josan <josanescod@gmail.com>
2021-07-01 08:23:30 +0200

    nodeAPI

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -62,6 +62,6 @@ app.post("/failure",function (req,res){
     res.redirect("/");
 })
 
-app.listen(process.env.PORT,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log(`Server listening on port ${process.env.PORT}`);
 })
