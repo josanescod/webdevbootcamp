50e5fadff8abed4c0b0010e619d3a19e631b57c5
josan <josanescod@gmail.com>
2021-07-02 08:01:14 +0200

    node ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,21 +2,25 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
 
     let today = new Date();
-    let currentDay = today.getDay();
+    let currentDay = today.getDay(); 
+    let day = "";
 
     if(currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
         res.send("<h1>Yay it's the weekend</h1>");
     }else {
-        res.sendFile(__dirname + "/index.html");
-    }
-    
+        day = "Weekday";
+        res.render("list",{ kindOfDay:day });
+    }   
 
 });
-
 
 app.listen(process.env.PORT,function(){
     console.log(`Server started on port ${process.env.PORT}`);
 })
+
