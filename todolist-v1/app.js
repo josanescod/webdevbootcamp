b5d8bbdcb1b7620bb45d6e9ab3782b625bedd9dc
josan <josanescod@gmail.com>
2021-07-08 08:51:48 +0200

    templating/layouts

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
-let items = ["Buy Food","Cook Food","Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
-
 
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));//POST
 
 app.get("/", function (req, res) {
-   
+
     let today = new Date();
     let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-    let day = today.toLocaleDateString("en-US",options);    
-    res.render("list", { listTitle: day, newListItems: items});
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
-app.post("/",function(req,res){
+app.post("/", function (req, res) {
 
     let item = req.body.newItem;
 
@@ -29,30 +28,28 @@ app.post("/",function(req,res){
         workItems.push(item)
         res.redirect("/work");
 
-    }else {
+    } else {
         items.push(item);
         res.redirect("/");
 
     }
-    
-    
+})
 
+app.get("/work", function (req, res) {
+
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
 
 })
 
-app.get("/work",function(req,res){
-    res.render("list", { listTitle: "Work List", newListItems: workItems});
+app.get("/about", function (req, res) {
 
-
-})
-
-app.get("/about",function(req,res){
     res.render("about");
 })
 
 
 
 app.listen(process.env.PORT, function () {
+    
     console.log(`Server started on port ${process.env.PORT}`);
 })
 
