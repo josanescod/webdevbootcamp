4028a901642961c9a638e2bde6f8fd64f0cd5086
josan <josanescod@gmail.com>
2021-07-11 08:56:51 +0200

    ejs

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -10,6 +10,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true})); //POST
@@ -38,8 +39,11 @@ app.post("/compose",function(req,res){
     title : req.body.postTitle,
     content : req.body.postBody,
   }
+
+  posts.push(post);
+  console.log(posts);
   
-  res.redirect("/compose");
+  res.redirect("/");
 })
 
 
