6923e8d5452a286e34c3b1d205298b650ab011b2
josan <josanescod@gmail.com>
2021-07-14 12:00:35 +0200

    mongoDB

--- fruits-project/app.js
+++ fruits-project/app.js
@@ -11,12 +11,12 @@ async function main(){
         await client.connect();
         await console.log("connection successfully");
         //await listDatabases(client);
-        await createListing(client, {
+        /*await createListing(client, {
             _id:2,
             name: "Lemon",
             units: 10
-        })
-
+        })*/
+        await findOneListingByName(client, 'Lemon');
     } catch(e) {
         console.error(e);
     } finally {
@@ -28,18 +28,29 @@ async function main(){
 main().catch(console.error);
 
 async function createListing(client,newListing) {
-   const result = await client.db("sample_fruit").collection("acidFruits").insertOne(
+   const result = await client.db("fruitsDB").collection("acidFruits").insertOne(
         (newListing)  );
 
     console.log(`New listing created with following id: ${result.insertedId}`);
 }
 
+async function findOneListingByName (client, nameOfListing) {
+    const result = await client.db("fruitsDB").collection("acidFruits").findOne({
+        nameOfListing
+    })
+
+    if (result) {
+        console.log(`Found a listing in the collection with the name ${nameOfListing}`)
+        console.log(result);
+    }else {
+        console.log(`No listings found with the name ${nameOfListing}`)
+    }
+}
 
 
 
 async function listDatabases(client){
     const databasesList = await client.db().admin().listDatabases();
-
     console.log("Databases");
     databasesList.databases.forEach(db => { console.log(`- ${db.name}`);
         
