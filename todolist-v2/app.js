8e1df5cbc98d9b4ca7946144e5870bcc48fcd339
josan <josanescod@gmail.com>
2021-07-18 14:06:19 +0200

    moongose todolistv2

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -6,11 +6,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //create new database
-mongoose.connect(`${process.env.MONGODB}`,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`${process.env.MONGODB}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //create new Schema
 const itemsSchema = new mongoose.Schema({
@@ -36,44 +36,55 @@ const item3 = new Item({
 
 })
 
-const defaultItems = [item1,item2,item3];
-
-Item.insertMany(defaultItems, function (err) {
-  if (err) {
-    console.log(err);
-  }else {
-    console.log("Succesfuly saved default items to DB.")
-  }
-})
+const defaultItems = [item1, item2, item3];
 
 
-app.get("/", function(req, res) {
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+app.get("/", function (req, res) {
 
+  //read querys
+  Item.find(function (err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Succesfuly saved default items to DB.")
+        }
+      });
+      res.redirect("/")
+
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  })
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-  } else {
-    items.push(item);
+  } else {    
+    const item = new Item({
+      name: itemName    
+    });
+    item.save();
     res.redirect("/");
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT, function () {
   console.log(`Server started on port ${process.env.PORT}`);
 });
