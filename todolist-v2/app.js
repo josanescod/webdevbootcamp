27886b8cb987b02238779d62f5d5090671d53c3b
josan <josanescod@gmail.com>
2021-07-18 12:08:19 +0200

    moongose todolistv2

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -1,24 +1,50 @@
-//jshint esversion:6
-
 const express = require("express");
-const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+//create new database
+mongoose.connect(`${process.env.MONGODB}`,{useNewUrlParser: true})
+
+//create new Schema
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+//create Model name of collection in singular and Schema
+const Item = moongose.model("Item", itemsSchema)
+
+//create three new documents
+const run10km = {
+  name: "Run 10 km"
+
+}
+
+const studyJS = {
+  name: "Study JS 1 hour"
+
+}
+
+const feedCat = {
+  name: "Feed the cat"
+
+}
+
+run10km.save();
+studyJS.save();
+feedCat.save();
+
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
@@ -43,6 +69,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, function() {
+  console.log(`Server started on port ${process.env.PORT}`);
 });
