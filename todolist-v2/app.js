27f8ac2a2faffe56f886431639baca1a38d61760
josan <josanescod@gmail.com>
2021-07-23 14:42:49 +0200

    add gitignore, procfile and update ports

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -152,6 +152,13 @@ app.post("/delete", function (req, res) {
 });
 
 
-app.listen(process.env.PORT, function () {
-  console.log(`Server started on port ${process.env.PORT}`);
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log(`Server has started Successfully`);
 });
