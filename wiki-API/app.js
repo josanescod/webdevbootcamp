604f4a277756751e7cfe51b3f6e4690108a2610a
josan <josanescod@gmail.com>
2021-07-25 19:32:42 +0200

    api rest

---
+++ wiki-API/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const ejs = require("ejs");
+require('dotenv').config();
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+//create new database
+mongoose.connect(`${process.env.MONGODB}`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//new schema
+const wikiSchema = new mongoose.Schema({
+    title: String,
+    content: String
+
+});
+
+//new model
+const Article = mongoose.model("Article", wikiSchema);
+
+
+
+app.listen(process.env.PORT, function () {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
