93b5725c6c85a48049a0cb56bc14c6d0fbbaa63a
josan <josanescod@gmail.com>
2021-07-26 19:03:18 +0200

    api rest

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -62,6 +62,54 @@ app.route("/articles")
         })
     });
 
+app.route("/articles/:articleTitle")
+
+    .get(function (req, res) {
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No articles matching that title was found.");
+            }
+        });
+    })
+    .put(function(req,res){       
+        Article.updateOne(
+            {title: req.params.articleTitle },
+            {$set: {title: req.body.title, content: req.body.content}},{overwrite: true}, 
+            function(err){
+                if (!err) {
+                    res.send("Successfully updated article.");
+                } 
+        });
+
+    })
+
+    .patch(function(req,res){        
+        Article.updateOne(
+            {title: req.params.articleTitle },
+            {$set: req.body}, 
+            function(err){
+                if (!err) {
+                    res.send("Successfully updated article.");
+                } else{
+                    res.send(err);
+                }
+        });
+
+    })
+    .delete(function(req,res){
+        Article.deleteOne({ title: req.params.articleTitle }, function (err) {
+            if (!err) {
+                res.send("Succesfully deleted article.");
+            } else {
+                res.send(err);
+            }
+        });
+
+    });
+
+
 app.listen(process.env.PORT, function () {
     console.log(`Server started on port ${process.env.PORT}`);
 });
