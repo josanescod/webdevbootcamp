ee9c254d8bb6ca359b5df2a4ca7e2f9173dc9ec2
josan <josanescod@gmail.com>
2021-08-06 12:36:24 +0200

    JSX Code Practice

--- react-example/src/index.js
+++ react-example/src/index.js
@@ -2,38 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const name = "Joseph Sanx";
-const currentYear = new Date().getFullYear();
-let copy =  "\u00A9";
-const img = "https://picsum.photos/200/300";
+const date = new Date(2019,1,1,12);
+const currentTime = date.getHours();
 
-let customStyle = {
-  color:"red",
-  fontSize: "20px",
-  border: "1px solid black",
-  marginRight: "70%",
-  alignText: "center"
+let greeting;
+
+const customStyle = {
+  color: ""
 }
-customStyle.color = "blue";
+
+if (currentTime < 12){
+  greeting = "Good Morning";
+  customStyle.color = "red";
+  
+}else if (currentTime < 18){
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+  
+}else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+
+}
+
 
 ReactDOM.render(
-  
-      <footer>
-        <h1 className="heading">React tutorial</h1>
-          <p>
-          Created by {name}
-          </p>
-          <img className="logo-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/8/83/OpenBSD_Logo_-_Cartoon_Puffy_with_textual_logo_below.svg/1024px-OpenBSD_Logo_-_Cartoon_Puffy_with_textual_logo_below.svg.png" alt="openBSD logo"/>
-          <br/>
-          <img className="logo-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Freebsd_logo.svg/1920px-Freebsd_logo.svg.png" alt="FreeBSD logo"/>
-          <br/>
-          <img className="logo-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/249px-Tux.svg.png" alt="Linux logo"/>
-          <br/>
-          <img className="logo-img" src={img+ '?grayscale'}/>
-          <p style={customStyle}>
-            Copyright <span>{copy}</span> {currentYear}
-          </p>
-    </footer>
+
+  <div>
+    <h1 className="heading" style={customStyle}> {greeting} {currentTime}</h1>
+  </div>
   ,
 
   document.getElementById("root"));
