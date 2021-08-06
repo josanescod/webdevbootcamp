88b954721bddf6c633bf35cb78a548fb26369b39
josan <josanescod@gmail.com>
2021-08-06 09:13:46 +0200

    JSX Code Practice

--- react-example/src/index.js
+++ react-example/src/index.js
@@ -2,33 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const name = "Jhon";
-const lastName = "Smith";
-const luckyNumber = 8;
+const name = "Joseph Sanx";
+const currentYear = new Date().getFullYear();
+let copy =  "\u00A9";
 
 ReactDOM.render(
-  <div>
-    <h1>React Tutorial</h1>
-    <h2>Hello {name} {lastName}</h2> {/* adding expresions and comments */}
-    <p>Food</p>
-    <ul>
-      <li>
-        Bacon
-      </li>
-      <li>
-        Noodles
-      </li>
-      <li>
-        Jamon
-      </li>
-    </ul>
-    <p>Your lucky number is {luckyNumber + 4}</p>
-    <p>Random number {Math.floor(Math.random()*10)}</p>
-    <p>{
-      
-      
-      }</p>
-  </div>,
+  <body>
+      <footer>
+          <p>
+          Created by {name}
+          </p>
+          <p>
+            Copyright <span>{copy}</span> {currentYear}
+          </p>
+    </footer>
+  </body>,
 
   document.getElementById("root"));
 
