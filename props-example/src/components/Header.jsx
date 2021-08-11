2804d57e7d47ee7b1c27e8e0532958340064ab09
josan <josanescod@gmail.com>
2021-08-11 12:41:19 +0200

    props

---
+++ props-example/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <h1>My Contacts</h1>
+    );
+}
+
+
+export default Header;
---
+++ props-example/src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+
+    {
+        name: "Gambit",
+        imgURL: "https://sm.ign.com/t/ign_es/feature/g/gambit-exp/gambit-explained-who-is-the-x-men-character_1nw3.1280.jpg",
+        phone: "12345678",
+        email: "gambit@mail.dev"
+    },
+    {
+        name: "C.Xavier",
+        imgURL: "https://www.looper.com/img/gallery/professor-xs-entire-backstory-explained/intro-1587748942.jpg",
+        phone: "00000192",
+        email: "px@mail.dev"
+
+    },
+    {
+        name: "Storm",
+        imgURL: "https://img.vixdata.io/pd/jpg-large/es/sites/default/files/btg/comics.batanga.com/files/Los-mejores-X-Men-de-la-historia-segun-Marvel-8.jpg",
+        phone: "00671291",
+        email: "storm@mail.dev"
+    }
+]
+
+export default contacts;
