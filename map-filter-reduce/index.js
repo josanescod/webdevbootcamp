61ea6f6e1811c0c9681b7c2c2ecfa330a3fe916b
josan <josanescod@gmail.com>
2021-08-13 09:26:44 +0200

    import export require

--- map-filter-reduce/index.js
+++ map-filter-reduce/index.js
@@ -1,30 +1,7 @@
-const emojipedia = [
-    {
-        id: 1,
-        emoji: "💪",
-        name: "Tense Biceps",
-        meaning: "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-    },
-    {
-        id: 2,
-        emoji: "🙏",
-        name: "Person With Folded Hands",
-        meaning: "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
-    },
-    {
-        id: 3,
-        emoji: "🤣",
-        name: "Rolling On The Floor, Laughing",
-        meaning: "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-    },
-    {
-        id: 4,
-        emoji: "🤓",
-        name: "Nerd Face",
-        meaning: "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
-    }
-];
-import emojipedia from '../emojipedia';/* Map Filter Reduce*/
+
+//const emojipedia = require('./emojipedia.js');
+import emojipedia from './emojipedia.mjs';
+/* Map Filter Reduce*/
 var numbers = [3, 56, 2, 48, 5];
 
 //Map create a new array by doing something with each item in an array
