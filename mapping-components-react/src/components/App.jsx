72c4fab81bc2b4b58e2565e5a3fed2c7dc01c513
josan <josanescod@gmail.com>
2021-08-13 08:31:58 +0200

    refactor fat arrow

--- mapping-components-react/src/components/App.jsx
+++ mapping-components-react/src/components/App.jsx
@@ -3,16 +3,7 @@ import Entry from './Entry';
 import emojipedia from '../emojipedia';
 
 
-function createEntry(emojiTerm) {
-    return (
-        <Entry
-            key={emojiTerm.id}
-            emoji={emojiTerm.emoji}
-            name={emojiTerm.name}
-            description={emojiTerm.meaning}
-        />
-    );
-}
+
 
 function App() {
 
@@ -21,7 +12,10 @@ function App() {
             <h1>
                 <span>emojipedia</span>
             </h1>
-            <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+            <dl className="dictionary">{emojipedia.map(
+                emojiTerm => (
+                    <Entry key={emojiTerm.id} emoji={emojiTerm.emoji} name={emojiTerm.name} description={emojiTerm.meaning} />))}
+            </dl>
         </div>
     );
 }
