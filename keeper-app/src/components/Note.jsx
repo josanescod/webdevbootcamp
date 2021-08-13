f4f6ebaf659ab9cafd93abf6fa01741ca515bae6
josan <josanescod@gmail.com>
2021-08-13 15:53:03 +0200

    keeper map props

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 
 
-function Note(){
+function Note(props) {
 
-    return(
-       <div className = "note"> 
-            <h1>This is the note title</h1> 
-            <br/>
-            <p>This is the note content</p>  
-       </div>
+    return (
+        <div className="note">
+            <h1>{props.title}</h1>
+            <br />
+            <p>{props.content}</p>
+        </div>
     );
 }
 
