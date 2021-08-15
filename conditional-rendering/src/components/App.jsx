8b98243b039100b64cc72f3d265fb2763e6b0500
josan <josanescod@gmail.com>
2021-08-15 09:33:06 +0200

    conditional-rendering

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import Login from './Login';
+import Form from './Form';
 
-var isLoggedIn = false;
-const currentTime = new Date(2019, 11, 1, 13).getHours();
+var userIsRegistered = false
+
 
 function App() {
     return (
         <div className="container">
             {
-                isLoggedIn === true ? <h1>Hello</h1> : <Login />
-            }
-
-            {
-                //currentTime > 12 ? <h1>Why are you still working?</h1> : null
-                currentTime > 12 && <h1>Why are you still working?</h1> //AND operator
+                <Form
+                    isRegistered={userIsRegistered}
+                />
             }
         </div>
     );
