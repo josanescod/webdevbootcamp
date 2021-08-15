8c0347f5e6e0140da45fbb0c2220eaa967659dbe
josan <josanescod@gmail.com>
2021-08-15 09:02:09 +0200

    conditional-rendering

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React from 'react';
+import Login from './Login';
+
+var isLoggedIn = false;
+const currentTime = new Date(2019, 11, 1, 13).getHours();
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            {
+                isLoggedIn === true ? <h1>Hello</h1> : <Login />
+            }
+
+            {
+                //currentTime > 12 ? <h1>Why are you still working?</h1> : null
+                currentTime > 12 && <h1>Why are you still working?</h1> //AND operator
+            }
+        </div>
+    );
 }
 
 export default App;
---
+++ conditional-rendering/src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Input from './Input';
+
+function Login() {
+    return (
+        <form className="form">
+            <Input
+                key='1'
+                type="text"
+                placeholder="Username"
+            />
+            <Input
+                key='2'
+                type="password"
+                placeholder="Password"
+            />
+            <button type="submit">Login</button>
+        </form>
+    )
+}
+
+export default Login;
