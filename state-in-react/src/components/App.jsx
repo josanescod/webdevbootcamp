b9bd4ef13d113575a05c0a5a25ac1689c20ec3aa
josan <josanescod@gmail.com>
2021-08-16 07:57:32 +0200

    state in react

--- state-in-react/src/components/App.jsx
+++ state-in-react/src/components/App.jsx
@@ -1,57 +1,28 @@
-import React, { useState } from "react";
-import ToDoItem from "./ToDoItem";
+import React from "react";
+
+var isDone = true;
+
+function strike() {
+    isDone = true;
+
+}
+function unstrike() {
+    isDone = false;
+
+}
+
+const lineThrough = { texDecoration: "line-through" };
 
 function App() {
-    const [todos, setTodos] = useState([]);
-    const [input, setInput] = useState("");
-
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInput(newValue);
-    }
-
-    function addItem() {
-        setTodos(prevTodos => {
-            console.log(input);
-            return [...prevTodos, input];
-        });
-        setInput("");
-        console.log(todos);
-    }
-
-    function removeItem(id) {
-        setTodos(prevTodos => {
-            return prevTodos.filter((item, index) => {
-                return index !== id;
-            });
-        });
-    }
-
     return (
-        <div className="container">
-            <div className="heading">
-                <h1>To-Do List</h1>
-            </div>
-            <div className="form">
-                <input type="text" value={input} onChange={handleChange} />
-                <button onClick={addItem}>
-                    <span>Add</span>
-                </button>
-            </div>
-            <div>
-                <ul>
-                    {todos.map((todoItem, index) => (
-                        <ToDoItem
-                            key={index}
-                            id={index}
-                            text={todoItem}
-                            onChange={removeItem}
-                        />
-                    ))}
-                </ul>
-            </div>
+        <div>
+            <p style={isDone ? lineThrough : null}>Buy milk</p>
+            <button onClick={strike}>Change to strike through </button>
+            <br />
+            <button onClick={unstrike}>Change back </button>
         </div>
-    );
+    )
+
 }
 
 export default App;
